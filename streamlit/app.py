bcdad2db57c6525baccc7f8489648f71d1e92237
Jose Salerno <jsale017@bu.edu>
2024-11-05 17:31:38 +0000

    whats up with gitignore huh?

--- streamlit/app.py
+++ streamlit/app.py
@@ -9,7 +9,6 @@ credentials = service_account.Credentials.from_service_account_info(
     st.secrets["service_account_key"] if "service_account_key" in st.secrets else {}
 )
 
-
 # Initialize the BigQuery client with credentials and project ID
 client = bigquery.Client(credentials=credentials, project=credentials.project_id)
 
