166a4bf4072c8d9c87462937b3b172acdcdd81d4
Jose Salerno <jsale017@bu.edu>
2024-11-14 14:05:29 +0000

    adding deploying logic for ML tasks

--- flows/deploy-etl.py
+++ flows/deploy-etl.py
@@ -2,20 +2,24 @@ from prefect import flow
 
 if __name__ == "__main__":
     flow.from_source(
-        source="https://github.com/jsale017/Predictive-Financial-Analytics-APIs.git",
-        entrypoint="Alpha_Vantage_Pipeline/flows/etl.py:etl_flow",
-    ).deploy(
-        name="stock-etl-flow-v2-local-test",
-        work_pool_name="Finance Local Test",
-        job_variables={
-            "env": {"PROJECT_ID": "finnhub-pipeline-ba882"},
-            "pip_packages": [
-                "pandas", "requests", "google-cloud-storage", 
-                "google-cloud-secret-manager", "google-cloud-bigquery"
-            ]
+    source="https://github.com/jsale017/Predictive-Financial-Analytics-APIs.git",
+    entrypoint="flows/etl.py:etl_flow",
+).deploy(
+    name="stock-etl-flow-v2-local-test",
+    work_pool_name="Finance Alpha Vantage",
+    job_variables={
+        "env": {
+            "PROJECT_ID": "finnhub-pipeline-ba882",
+            "GOOGLE_APPLICATION_CREDENTIALS": "/path/to/service_account_key.json"
         },
-        cron="0 21 * * *",
-        tags=["prod"],
-        description="ETL flow for processing stock market data from Alpha Vantage",
-        version="1.0.0",
-    )
+        "pip_packages": [
+            "pandas", "requests", "google-cloud-storage",
+            "google-cloud-secret-manager", "google-cloud-bigquery"
+        ]
+    },
+    cron="0 21 * * *",
+    tags=["prod"],
+    description="ETL flow for processing stock market data from Alpha Vantage",
+    version="1.0.0",
+)
+
