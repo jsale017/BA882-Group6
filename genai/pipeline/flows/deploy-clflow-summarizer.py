3636af971a8e9953ce9151810499aeb96e99fd64
Jose Salerno <jsale017@bu.edu>
2024-12-05 21:39:53 +0000

    fixing deploy-clflow file

--- genai/pipeline/flows/deploy-clflow-summarizer.py
+++ genai/pipeline/flows/deploy-clflow-summarizer.py
@@ -1,22 +1,22 @@
-flow.from_source(
-    source="https://github.com/jsale017/Predictive-Financial-Analytics-APIs.git",
-    entrypoint="genai/pipeline/flows/summary-rewriter.py:summarizer_flow",
-).deploy(
-    name="genai-financial-advisor",
-    work_pool_name="Finance Alpha Vantage",
-    job_variables={
-        "env": {"PROJECT_ID": "finnhub-pipeline-ba882"},
-        "pip_packages": [
-            "pandas",
-            "google-cloud-bigquery",
-            "google-auth",
-            "controlflow",
-            "prefect[gcp]",
-            "langchain-google-vertexai"
-        ]
-    },
-    cron="15 2 * * *",
-    tags=["prod"],
-    description="Pipeline to process posts from BigQuery",
-    version="1.0.0",
-)
+from prefect import flow
+
+if __name__ == "__main__":
+    flow.from_source(
+        source="https://github.com/jsale017/Predictive-Financial-Analytics-APIs.git",
+        entrypoint="genai/pipeline/flows/summary-rewriter.py:summarizer_flow",
+    ).deploy(
+        name="genai-financial-advisor",
+        work_pool_name="Finance Alpha Vantage",
+        job_variables={"env": {"PROJECT_ID": "finnhub-pipeline-ba882"},
+                       "pip_packages": ["pandas", 
+                                        "google-cloud-bigquery", 
+                                        "controlflow",
+                                        "prefect[gcp]",
+                                        "google-auth",
+                                        "langchain-google-vertexai"]},
+        cron="15 2 * * *",
+        tags=["prod"],
+        description="Pipeline to process posts from BigQuery",
+        version="1.0.0",
+    )
+
