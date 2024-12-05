af5056bad25292a312928c0ac52cef52e1a8b89b
Jose Salerno <jsale017@bu.edu>
2024-12-05 21:50:18 +0000

    chaning the orchestraction of the news

---
+++ news_etl/orchestration/news_etl_flow.py
@@ -0,0 +1,34 @@
+from prefect import flow
+from prefect.events import DeploymentEventTrigger
+
+if __name__ == "__main__":
+    flow.from_source(
+        source="https://github.com/jsale017/Predictive-Financial-Analytics-APIs.git",
+        entrypoint="news_etl/orchestration/news_etl_flow.py:news_etl_flow",
+    ).deploy(
+        name="news-etl-pipeline",
+        work_pool_name="Finance Alpha Vantage",
+        job_variables={
+            "env": {
+                "PROJECT_ID": "finnhub-pipeline-ba882",
+                "NEWS_GCF_URLS": {
+                    "extract": "https://us-central1-finnhub-pipeline-ba882.cloudfunctions.net/extract_news",
+                    "transform": "https://us-central1-finnhub-pipeline-ba882.cloudfunctions.net/transform_news",
+                    "load": "https://us-central1-finnhub-pipeline-ba882.cloudfunctions.net/load_news",
+                },
+            },
+            "pip_packages": [
+                "pandas", "requests", "google-cloud-storage",
+                "google-cloud-secret-manager", "google-cloud-bigquery", "prefect"
+            ]
+        },
+        tags=["prod"],
+        description="News ETL Pipeline for extracting, transforming, and loading news data into BigQuery.",
+        version="1.0.0",
+        triggers=[
+            DeploymentEventTrigger(
+                expect={"prefect.flow-run.Completed"},
+                match_related={"prefect.resource.name": "news-etl-pipeline-test"}
+            )
+        ]
+    )
