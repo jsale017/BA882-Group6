ef50af8bc874d431d75f3d45d6974f78d2f5861a
Jose Salerno <jsale017@bu.edu>
2024-12-05 21:32:33 +0000

    new inclusion multi-agent

---
+++ genai/pipeline/flows/deploy-clflow-summarizer.py
@@ -0,0 +1,22 @@
+flow.from_source(
+    source="https://github.com/jsale017/Predictive-Financial-Analytics-APIs.git",
+    entrypoint="genai/pipeline/flows/summary-rewriter.py:summarizer_flow",
+).deploy(
+    name="genai-financial-advisor",
+    work_pool_name="Finance Alpha Vantage",
+    job_variables={
+        "env": {"PROJECT_ID": "finnhub-pipeline-ba882"},
+        "pip_packages": [
+            "pandas",
+            "google-cloud-bigquery",
+            "google-auth",
+            "controlflow",
+            "prefect[gcp]",
+            "langchain-google-vertexai"
+        ]
+    },
+    cron="15 2 * * *",
+    tags=["prod"],
+    description="Pipeline to process posts from BigQuery",
+    version="1.0.0",
+)
